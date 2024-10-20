699537ba394da2ba34e664edbace7516ce90292e
Jehyeok Yeon (Tommy) <69325815+Jeybird248@users.noreply.github.com>
2024-10-20 10:55:59 -0500

    Title: 세 수의 합, Time: 119ms, Memory: 25MB, Status: Passed - CodeTree

--- 241021/세 수의 합/sum-of-three-num.py
+++ 241021/세 수의 합/sum-of-three-num.py
@@ -1,17 +1,24 @@
 n, k = map(int, input().split())
 
+
 arr = list(map(int, input().split()))
 
-count = 0
-d = {}
+count = dict()
+for elem in arr:
+    if elem in count:
+        count[elem] += 1
+    else:
+        count[elem] = 1
+
+ans = 0
+
 for i in range(n):
-    target = k - arr[i]
-    seen = set()
+    count[arr[i]] -= 1
 
-    for j in range(i + 1, n):
-        complement = target - arr[j]
-        if complement in seen:
-            count += 1
-        seen.add(arr[j])
+    for j in range(i):
+        diff = k - arr[i] - arr[j]
 
-print(count)
+        if diff in count:
+            ans += count[diff]
+
+print(ans)
