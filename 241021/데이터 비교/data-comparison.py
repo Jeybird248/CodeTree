739a7fc44fbb81ab88c1a95108e61e5fdb337a42
Jehyeok Yeon (Tommy) <69325815+Jeybird248@users.noreply.github.com>
2024-10-21 09:54:04 -0500

    Title: 데이터 비교, Time: 5015ms, Memory: 35MB, Status: Time Limit Exceed - CodeTree

--- 241021/데이터 비교/data-comparison.py
+++ 241021/데이터 비교/data-comparison.py
@@ -4,8 +4,8 @@ arr1 = list(map(int, input().split()))
 m = int(input())
 arr2 = list(map(int, input().split()))
 
-for num in arr1:
-    if num in arr2:
+for num in arr2:
+    if num in arr1:
         print(1, end = " ")
     else:
         print(0, end = " ")
