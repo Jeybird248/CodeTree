081467b5cd624d8d0b49762ec061d14b8abcfcb0
Jehyeok Yeon (Tommy) <69325815+Jeybird248@users.noreply.github.com>
2024-10-22 01:47:37 -0500

    Title: 정수 찾기, Time: 177ms, Memory: 36MB, Status: Passed - CodeTree

--- 241022/정수 찾기/find-an-integer.py
+++ 241022/정수 찾기/find-an-integer.py
@@ -1,10 +1,17 @@
+# Variable declaration and input:
 n = int(input())
-arr1 = set(map(str, input().split()))
+arr1 = list(map(int, input().split()))
 m = int(input())
-arr2 = set(map(str, input().split()))
+arr2 = list(map(int, input().split()))
 
-for i in arr2:
-    if i in arr1:
-        print(1)
-    else:
+# Insert all elements of sequence a into a HashSet.
+set1 = set(arr1)
+
+# Check if each element of sequence b is in sequence a.
+for elem2 in arr2:
+    # If not found, output 0.
+    if elem2 not in set1:
         print(0)
+    # If found, output 1.
+    else:
+        print(1)
