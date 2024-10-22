e1072cf963bb92713c1ce7f76549999711a798a5
Jehyeok Yeon (Tommy) <69325815+Jeybird248@users.noreply.github.com>
2024-10-22 02:35:52 -0500

    Title: 초대장과 번호표, Time: 196ms, Memory: 32MB, Status: Passed - CodeTree

--- 241022/초대장과 번호표/invitation-and-number-tag.py
+++ 241022/초대장과 번호표/invitation-and-number-tag.py
@@ -1,28 +1,30 @@
 N, G = map(int, input().split())
-groups = [[] for _ in range(N+1)]
+groups = [[] for _ in range(G)]
+person_in_groups = [[] for _ in range(N+1)]
+uninvited_count = [0] * G
 
 for i in range(G):
     arr = list(map(int, input().split()))[1:]
+    groups[i] = arr
+    uninvited_count[i] = len(arr)
     for num in arr:
-        groups[num].append(i)
+        person_in_groups[num].append(i)
 
 q = [1]
-visited_groups = set()
-visited_elements = set([1])
+visited_people = set([1])
 answer = 0
 
 while q:
-    temp = q.pop(0)
+    person = q.pop(0)
     answer += 1
 
-    for group in groups[temp]:
-        if group in visited_groups:
-            continue
-        visited_groups.add(group)
-
-        for num in groups[temp]:
-            if num not in visited_elements:
-                visited_elements.add(num)
-                q.append(num)
+    for group in person_in_groups[person]:
+        uninvited_count[group] -= 1 
+        
+        if uninvited_count[group] == 1: 
+            for member in groups[group]:
+                if member not in visited_people:
+                    visited_people.add(member)
+                    q.append(member)
 
 print(answer)
